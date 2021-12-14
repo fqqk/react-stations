b26f050a047b9be61b213e9e01c8857af9d1cc25
fqqk <fqqk.work68@gmail.com>
2021-12-14 19:50:39 +0900

    station-7

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@
 import * as React from 'react'
 import './App.css'
 
-/**
- * 
- * @type {React.FC}
- */
+// /**
+//  * 
+//  * @type {React.FC}
+//  */
 export const App = () => {
+
+  const [dogUrl,setDogUrl] = React.useState("https://images.dog.ceo/breeds/akita/Akita_inu_blanc.jpg");
+
+
+  const updateImg = async function() {
+    const res = await fetch("https://dog.ceo/api/breeds/image/random");
+    const data = await res.json();
+      if(data.status === "success"){
+        // console.log(data.message);
+        setDogUrl(data.message);
+      }
+  }
+
+
   return (
-    <div>
-      <h2>Hello, world!</h2>
+    <div className = "container">
+      <header>
+        <h2>Dog アプリ</h2>
+      </header>
+      <main>
+        <section className = "dogView">
+        <div className = "flex">
+          <div className = "item_des"><p>犬の画像を表示するサイトです</p></div>
+          <div className = "item_img">
+            <img src = {dogUrl}></img>
+            <button onClick = {updateImg} className = "btn">更新</button>
+          </div>
+        </div>
+        </section>
+      </main>
+      
     </div>
   )
 }
